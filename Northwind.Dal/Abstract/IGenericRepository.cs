463492f169ed53e949d8a957a8b35ab2f6a02149
Deniz Selçuk <42787488+DenizSelcuk@users.noreply.github.com>
2021-12-25 14:54:53 +0300

    Add files via upload

---
+++ Northwind.Dal/Abstract/IGenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using Northwind.Entity.IBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Dal.Abstract
+{
+    public interface GenericRepository<T ,TDto> where T: IEntityBase
+    {
+        List<TDto> GetAll();
+        List<TDto> GetAll(Expression<Func<T, bool>> expression);
+        List<TDto> Find(int id);
+        IQueryable<T> GetIQueryable();
+        TDto Add(TDto item);
+        Task<TDto> AddAsync(TDto item);
+        TDto Update(TDto item);
+        Task<TDto> UpdateAsync(TDto item);
+        bool Delete(TDto item);
+        bool DeleteById(int id);
+        Task<bool> DeleteByIdAsync(int id);
+        Task<bool> DeleteAsync(TDto item);
+
+    }
+}
